89b08e674b7cf499657617d3747dfe8100068ffa
Roberto Simonetti <robisim74@gmail.com>
2016-02-12 23:17:39 +0100

    Upgraded to beta.6

--- Angular2Localization/app.component.ts
+++ Angular2Localization/app.component.ts
@@ -1,3 +1,5 @@
+///<reference path="node_modules/angular2/typings/browser.d.ts"/>
+
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http'; // Http module.
