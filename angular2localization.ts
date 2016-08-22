a0aaf15864df57d7dd2aed9f82e451f5f20c893c
Roberto Simonetti <robisim74@gmail.com>
2016-08-22 14:50:41 +0200

    Add modules

--- angular2localization.ts
+++ angular2localization.ts
@@ -1,3 +1,14 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+// Services.
+import { LocaleService } from './src/services/locale.service';
+import { LocalizationService } from './src/services/localization.service';
+// Pipes.
+import { TranslatePipe } from './src/pipes/translate.pipe';
+import { LocaleDatePipe } from './src/pipes/locale-date.pipe';
+import { LocaleDecimalPipe, LocalePercentPipe, LocaleCurrencyPipe } from './src/pipes/locale-number.pipe';
+// Directives.
+import { LocaleNumberValidator } from './src/directives/locale-number-validator.directive';
+
 // Exports services, pipes & directives.
 export * from './src/services/localization.service';
 export * from './src/services/locale.service';
@@ -9,3 +20,52 @@ export * from './src/pipes/translate.pipe';
 export * from './src/pipes/locale-date.pipe';
 export * from './src/pipes/locale-number.pipe';
 export * from './src/directives/locale-number-validator.directive';
+
+// Modules.
+@NgModule({
+    declarations: [
+        LocaleDatePipe,
+        LocaleDecimalPipe,
+        LocalePercentPipe,
+        LocaleCurrencyPipe,
+        LocaleNumberValidator
+    ],
+    exports: [
+        LocaleDatePipe,
+        LocaleDecimalPipe,
+        LocalePercentPipe,
+        LocaleCurrencyPipe,
+        LocaleNumberValidator
+    ]
+})
+
+export class LocaleModule {
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: LocaleModule,
+            providers: [LocaleService]
+        };
+    }
+
+}
+
+@NgModule({
+    declarations: [
+        TranslatePipe
+    ],
+    exports: [
+        TranslatePipe
+    ]
+})
+
+export class LocalizationModule {
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: LocalizationModule,
+            providers: [LocalizationService]
+        };
+    }
+
+}
