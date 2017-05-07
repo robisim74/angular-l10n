57cc1dab2f114af96777b1c50f5b23cad342ca22
Roberto Simonetti <robisim74@gmail.com>
2017-05-07 10:32:48 +0200

    Build: stop building for errors during compilation

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ const NPM_DIR = `dist`;
 const MODULES_DIR = `${NPM_DIR}/modules`;
 const BUNDLES_DIR = `${NPM_DIR}/bundles`;
 
-echo('Start building...');
+echo(`Start building...`);
 
 rm(`-Rf`, `${NPM_DIR}/*`);
 mkdir(`-p`, `./${MODULES_DIR}`);
@@ -21,30 +21,33 @@ echo(`Start TSLint`);
 exec(`tslint --project ./tsconfig.json --type-check ./src/**/*.ts`);
 echo(chalk.green(`TSLint completed`));
 
-/* Aot compilation: ES2015 sources */
+/* AoT compilation: ES2015 sources */
 echo(`Start AoT compilation`);
-exec(`ngc -p tsconfig-build.json`);
+if (exec(`ngc -p tsconfig-build.json`).code !== 0) {
+    echo(chalk.red(`Error: AoT compilation failed`));
+    exit(1);
+}
 echo(chalk.green(`AoT compilation completed`));
 
 /* Creates bundles: ESM/ES5 and UMD bundles */
 echo(`Start bundling`);
 echo(`Rollup package`);
-exec(`rollup -i ${NPM_DIR}/index.js -o ${MODULES_DIR}/${PACKAGE}.js --sourcemap`, {silent: true});
+exec(`rollup -i ${NPM_DIR}/index.js -o ${MODULES_DIR}/${PACKAGE}.js --sourcemap`, { silent: true });
 exec(`node scripts/map-sources -f ${MODULES_DIR}/${PACKAGE}.js`);
 
 echo(`Downleveling ES2015 to ESM/ES5`);
 cp(`${MODULES_DIR}/${PACKAGE}.js`, `${MODULES_DIR}/${PACKAGE}.es5.ts`);
-exec(`tsc ${MODULES_DIR}/${PACKAGE}.es5.ts --target es5 --module es2015 --noLib --sourceMap`, {silent: true});
+exec(`tsc ${MODULES_DIR}/${PACKAGE}.es5.ts --target es5 --module es2015 --noLib --sourceMap`, { silent: true });
 exec(`node scripts/map-sources -f ${MODULES_DIR}/${PACKAGE}.es5.js`);
 rm(`-f`, `${MODULES_DIR}/${PACKAGE}.es5.ts`);
 
 echo(`Run Rollup conversion on package`);
-exec(`rollup -c rollup.config.js --sourcemap`, {silent: true});
+exec(`rollup -c rollup.config.js --sourcemap`, { silent: true });
 exec(`node scripts/map-sources -f ${BUNDLES_DIR}/${PACKAGE}.umd.js`);
 
 echo(`Minifying`);
 cd(`${BUNDLES_DIR}`);
-exec(`uglifyjs -c --screw-ie8 --comments -o ${PACKAGE}.umd.min.js --source-map ${PACKAGE}.umd.min.js.map --source-map-include-sources ${PACKAGE}.umd.js`, {silent: true});
+exec(`uglifyjs -c --screw-ie8 --comments -o ${PACKAGE}.umd.min.js --source-map ${PACKAGE}.umd.min.js.map --source-map-include-sources ${PACKAGE}.umd.js`, { silent: true });
 exec(`node ../../scripts/map-sources -f ${PACKAGE}.umd.min.js`);
 cd(`..`);
 cd(`..`);
