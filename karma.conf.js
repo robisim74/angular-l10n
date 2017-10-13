c4d7249602518a2ac226f26a3c2bc8fba1f2c950
Roberto Simonetti <robisim74@gmail.com>
2017-10-14 00:31:55 +0200

    Add Istanbul to report coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function (config) {
 
     const configuration = {
@@ -14,7 +16,9 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-webpack'),
             require('karma-sourcemap-loader'),
-            require('karma-spec-reporter')
+            require('karma-spec-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require("istanbul-instrumenter-loader")
         ],
 
         // list of files / patterns to load in the browser
@@ -41,8 +45,25 @@ module.exports = function (config) {
                 rules: [
                     {
                         test: /\.ts/,
-                        loaders: ['ts-loader', 'source-map-loader'],
+                        use: [
+                            { loader: 'ts-loader' },
+                            { loader: 'source-map-loader' }
+                        ],
                         exclude: /node_modules/
+                    },
+                    {
+                        enforce: 'post',
+                        test: /\.ts/,
+                        use: [
+                            {
+                                loader: 'istanbul-instrumenter-loader',
+                                options: { esModules: true }
+                            }
+                        ],
+                        exclude: [
+                            /\.spec.ts/,
+                            /node_modules/
+                        ]
                     }
                 ],
                 exprContextCritical: false
@@ -59,7 +80,13 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['spec'],
+        reporters: ['spec', 'coverage-istanbul'],
+
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly'],
+            dir: path.join(__dirname, 'coverage'),
+            fixWebpackSourcePaths: true
+        },
 
 
         // web server port
