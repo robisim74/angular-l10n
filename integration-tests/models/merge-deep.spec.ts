1383b41d6a3d86400b6947a83bd7e4e820de92c9
Unknown <fabien@dehopre.com>
2017-12-04 08:56:08 +0100

    rename tests for mergeDeep

--- integration-tests/models/merge-deep.spec.ts
+++ integration-tests/models/merge-deep.spec.ts
@@ -1,21 +1,21 @@
 import {mergeDeep} from "../../src/models/merge-deep";
 
 describe('mergeDeep', () => {
-    it('sould deeply merge to object correctly (case 1)', () => {
+    it('sould do the same as Object.assign on simple objects', () => {
         const target: any = { KEY1: 'Key1' };
         const source: any = { KEY2: 'Key2' };
         const result: any = mergeDeep(target, source);
-        expect(result).toEqual(jasmine.objectContaining({ KEY1: 'Key1', KEY2: 'Key2' }));
+        expect(result).toEqual(jasmine.objectContaining(Object.assign(target, source)));
     });
 
-    it('sould deeply merge to object correctly (case 2)', () => {
+    it('sould overwrite the same keys as Object.assign on simple objects', () => {
         const target: any = { KEY1: 'Key1' };
         const source: any = { KEY1: 'Key2' };
         const result: any = mergeDeep(target, source);
-        expect(result).toEqual(jasmine.objectContaining({ KEY1: 'Key2' }));
+        expect(result).toEqual(jasmine.objectContaining(Object.assign(target, source)));
     });
 
-    it('sould deeply merge to object correctly (case 3)', () => {
+    it('sould deeply merge objects correctly', () => {
         const target: any = { KEY1: 'Key1', SUBKEY1: { AA: 'aa' } };
         const source: any = { KEY2: 'Key2', SUBKEY2: { AA: 'aa'} };
         const result: any = mergeDeep(target, source);
@@ -31,7 +31,7 @@ describe('mergeDeep', () => {
         }));
     });
 
-    it('sould deeply merge to object correctly (case 4)', () => {
+    it('sould deeply merge objects correctly with same sub-key', () => {
         const target: any = { KEY1: 'Key1', SUBKEY1: { AA: 'aa' } };
         const source: any = { KEY2: 'Key2', SUBKEY1: { BB: 'bb'} };
         const result: any = mergeDeep(target, source);
@@ -45,7 +45,7 @@ describe('mergeDeep', () => {
         }));
     });
 
-    it('sould deeply merge to object correctly (case 5)', () => {
+    it('sould deeply merge objects correctly with same sub-key and overwite', () => {
         const target: any = { KEY1: 'Key1', SUBKEY1: { AA: 'aa' } };
         const source: any = { KEY2: 'Key2', SUBKEY1: { AA: 'aaa', BB: 'bb'} };
         const result: any = mergeDeep(target, source);
