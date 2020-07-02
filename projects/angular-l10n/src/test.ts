a229ffc7b9241808d7f993decf51ae32e997478d
Roberto Simonetti <robisim74@gmail.com>
2020-07-02 09:25:06 +0200

    CI: fix config file & add polyfills

--- projects/angular-l10n/src/test.ts
+++ projects/angular-l10n/src/test.ts
@@ -1,5 +1,13 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+/**
+ * Intl RelativeTimeFormat
+ */
+import 'intl-pluralrules';
+import '@formatjs/intl-relativetimeformat/polyfill';
+import '@formatjs/intl-relativetimeformat/dist/locale-data/en';
+import '@formatjs/intl-relativetimeformat/dist/locale-data/it';
+
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
