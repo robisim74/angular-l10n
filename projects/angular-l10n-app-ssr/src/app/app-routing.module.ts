2c91c62e4e4a79814f566cb014153f23c68d5b8b
Roberto Simonetti <robisim74@gmail.com>
2022-06-03 16:35:31 +0200

    Build: update packages

--- projects/angular-l10n-app-ssr/src/app/app-routing.module.ts
+++ projects/angular-l10n-app-ssr/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {
-            initialNavigation: 'enabled',
+            initialNavigation: 'enabledBlocking',
             relativeLinkResolution: 'corrected'
         })
     ],
