36e8e5260645b7a4f2039bc14bd5c6abceb9fdb3
Pascal Weyrich <pweyrich90@gmail.com>
2025-06-16 08:25:00 +0200

    fix: add fallback overload for l10n-translate pipe
    In my previous change (#351) I missed the fallback overload that matches the function signature. This introduces new type errors when upgrading to v17.

--- projects/angular-l10n/src/lib/pipes/l10n-translate.pipe.ts
+++ projects/angular-l10n/src/lib/pipes/l10n-translate.pipe.ts
@@ -15,6 +15,7 @@ export class L10nTranslatePipe implements PipeTransform {
     public transform(key: null, language: string, params?: any): null;
     public transform(key: "", language: string, params?: any): null;
     public transform(key: string, language: string, params?: any): string;
+    public transform(key: any, language: string, params?: any): string | null;
     public transform(key: any, language: string, params?: any): string | null {
         if (key == null || key === '') return null;
 
@@ -33,6 +34,7 @@ export class L10nTranslateAsyncPipe extends L10nAsyncPipe implements PipeTransfo
     public transform(key: null, params?: any, language?: string): null;
     public transform(key: "", params?: any, language?: string): null;
     public transform(key: string, params?: any, language?: string): string;
+    public transform(key: any, params?: any, language?: string): string | null;
     public transform(key: any, params?: any, language?: string): string | null {
         if (key == null || key === '') return null;
 
